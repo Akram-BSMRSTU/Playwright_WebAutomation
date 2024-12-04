f9e13c70354dca6934d07be9608078174bd43ea9
Akram Khan <akramkhanarafat12@gmail.com.com>
2024-12-04 12:46:19 +0600

    Implemented Date Picker

--- src/test/java/org/example/Datepicker.java
+++ src/test/java/org/example/Datepicker.java
@@ -1,17 +1,11 @@
 package org.example;
-
-import com.beust.ah.A;
 import com.microsoft.playwright.*;
-import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
-import java.nio.file.Paths;
-
-public class Fileupload {
-
-    String URL = "https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php";
+public class Datepicker {
+    String URL = "https://demo.automationtesting.in/Datepicker.html";
     Playwright playwright;
     BrowserType browserType;
     Browser browser;
@@ -30,18 +24,23 @@ public class Fileupload {
     @Test(priority = 0)
     public void openURL() throws InterruptedException {
         page.navigate(URL);
-        page.evaluate("document.body.style.zoom = '80%'");
+//        page.evaluate("document.body.style.zoom = '80%'");
+//        Thread.sleep(5000);
+    }
+
+    @Test(priority = 1)
+    public void DatepickerEnabled() throws InterruptedException {
+        ElementHandle element  = page.querySelector("//input[@id='datepicker2']");
+        element.fill("19/03/1999");
+        page.keyboard().press("Enter");
         Thread.sleep(5000);
     }
-//
-//    @Test(priority = 1)
-////    public void inputsinglefile() throws InterruptedException {
-////        String  filepath = "files/final logo jamuna.png";
-////        ElementHandle element  = page.querySelector("//input[@id='picture']");
-////        element.setInputFiles(Paths.get(filepath));
-////        assert element.contains("final logo jamuna.png");
-////        Thread.sleep(5000);
-////    }
+    @Test(priority = 2)
+    public void DatepickerDisabled() throws InterruptedException {
+//        ElementHandle element  = page.querySelector("//input[@id='datepicker1']");
+        page.evaluate("document.getElementById('datepicker1').value='12/27/1999'");
+        Thread.sleep(5000);
+    }
 
     @AfterSuite
     public void closeBrowser(){
@@ -49,4 +48,6 @@ public class Fileupload {
         browser.close();
         playwright.close();
     }
+
+
 }
